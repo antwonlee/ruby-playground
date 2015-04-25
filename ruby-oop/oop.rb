276e38bc6d5d2928fb90712146b04a8b748bae18
Anthony Lee <antwonlee@gmail.com>
2015-04-25 12:19:56 -0600

    simple ruby class

---
+++ ruby-oop/oop.rb
@@ -0,0 +1,17 @@
+# Resource: http://goo.gl/LTloEG
+
+# This is an object "Being"
+# Class definition (Currently it is empty)
+class Being
+
+end
+
+# Creating new instance of "Being" Class
+b = Being.new
+
+# We print the the object
+puts b
+
+# It will pring out the object name in terminal like this:
+#<Being:0x007fcd2608aca0>
+#<Being:0x007f8df408ab18>
