6ad7200e1363f873bf001cb74f6a9cb54243e3bb
Anthony Lee <antwonlee@gmail.com>
2015-04-25 12:33:15 -0600

    adding simple constructors

---
+++ ruby-oop/constructor.rb
@@ -0,0 +1,15 @@
+# Resource: http://goo.gl/LTloEG
+# Constructor initiates the state of an object.
+
+class Being
+  # initializing....
+  def initialize
+    puts "Being is created"
+  end
+end
+
+# Creating the instance.
+Being.new
+
+# Outputting...
+# >> Being is Created
