d3e70470c373990d7b5215ac9dfdb0351dc0fa76
Anthony Lee <antwonlee@gmail.com>
2015-08-12 00:06:25 -0600

    adding simple methods for todo item

--- simple-to-do-list-program/todo_item.rb
+++ simple-to-do-list-program/todo_item.rb
@@ -6,7 +6,23 @@ class TodoItem
     @complete = false
   end
 
+  def to_s
+    if complete?
+      "[C] #{name}"
+    else
+      "[I] #{name}"
+    end
+  end
+
+  def complete?
+    @complete
+  end
+
   def mark_complete!
     @complete = true
+  end
+
+  def mark_imcomplete!
+    @complete = false
   end
 end
