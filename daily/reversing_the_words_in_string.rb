f5cd8264bc7c06bca8cddf34741123296dd00a50
antwonlee <antwonlee@gmail.com>
2017-02-07 10:53:14 -0600

    Add simple reverse method :gem:

---
+++ daily/reversing_the_words_in_string.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+
+string = "Doctor Who"
+
+def sentence_reverser(string)
+  string.split.reverse.join(' ')
+end
+
+describe "Sentence reverser" do
+  it 'reverses the words in a sentence' do
+    test_sentence = "backwards am I"
+    expect(sentence_reverser(test_sentence)).to eq('I am backwards')
+  end
+end
