c0f2983087ed7b7db0473c542d1ca561773fc550
antwonlee <antwonlee@gmail.com>
2017-02-07 16:33:09 -0600

    Add more titles to the movives.csv :movie_camera:

--- daily/movies.rb
+++ daily/movies.rb
@@ -2,4 +2,19 @@ lines = File.open("movies.csv", "r") do |file_handle|
   file_handle.readlines
 end
 
-p lines
+rows = []
+lines.each do |line|
+  row << line.chomp.split(",")
+end
+
+movies = []
+headers = nil
+rows.each do |row|
+  if header.nil?
+    headers = row
+  else
+    movies << Hash[headers.zip(row)]
+  end
+end
+
+p movies.last
